3db97f6ffc0245a4d76849bea45797785564cec5
Angry_Pineapple <74722209+Angry-Pineapple3121@users.noreply.github.com>
2023-09-14 22:22:35 -0400

    make it prettier, add embeds, add buttons

--- cogs/quiz.py
+++ cogs/quiz.py
@@ -1,7 +1,9 @@
+import datetime
 import discord
 import random
 
 from discord.ext import commands
+from discord.commands import Option
 
 # Define all of our chromebook models
 MODELS = {
@@ -15,6 +17,29 @@ MODELS = {
     "HP G4 EE": "https://cdn.discordapp.com/attachments/704479038970003570/1151975946111418468/IMG_3702.jpg",
 }
 
+class RoleButton(discord.ui.Button):
+    def __init__(self, choice, correct_choice):
+        """A button for one role. `custom_id` is needed for persistent views."""
+        super().__init__(
+            label=choice,
+            style=discord.ButtonStyle.primary,
+            custom_id=choice  # Set a unique custom_id for each button
+        )
+
+        self.user_choice = choice
+        self.correct_choice = correct_choice
+    
+    async def callback(self, interaction: discord.Interaction):
+        if self.user_choice == self.correct_choice:
+            print(f'userChoice: {self.user_choice}')
+            print(f'correctChoice: {self.correct_choice}')
+            await interaction.response.send_message(f"Correct!", ephemeral=True)
+        else:
+            print(f'userChoice: {self.user_choice}')
+            print(f'correctChoice: {self.correct_choice}')
+            await interaction.response.send_message(f"Incorrect!", ephemeral=True)
+
+
 class Quiz(commands.Cog):
     print("==> Cog Loaded: Quiz")
     def __init__(self, bot):
@@ -26,26 +51,39 @@ class Quiz(commands.Cog):
 
         # Select our random model
         selected_model = random.choice(list(MODELS.keys()))
-    
         # Get the link for the selected model to display to user
         image_link = MODELS[selected_model]
-    
         # Create a list with all the model names
         all_models = list(MODELS.keys())
-    
         # Remove the correct answer from the list
         all_models.remove(selected_model)
-    
         # Select three random incorrect answers
         incorrect_answers = random.sample(all_models, 3)
-    
         # Combine correct and incorrect answers
         answers = [selected_model] + incorrect_answers
-    
         # Shuffle the answers
         random.shuffle(answers)
-        
-        await ctx.respond(f'which chromebook is this?\n{image_link}\n\nchoices:\n1. {answers[0]}\n2. {answers[1]}\n3. {answers[2]}\n4. {answers[3]}')
-        
+
+        # Create the embed
+        embed = discord.Embed(
+            title=f"<:slash:1150933397179486339> Cyber Flash Chromebook Quiz",
+            description=f'What is the **model** of the Chromebook shown below?',
+            color=discord.Colour.blurple(),
+        )
+        # Add the time the user is taking the quiz
+        now = datetime.datetime.now()
+        rtime = now.strftime("%B %d, %Y, %H:%M")
+        embed.set_footer(text=f"Requested by {ctx.author.display_name} » {rtime}")
+        # Set the image to the image being used for the quiz
+        embed.set_image(url=image_link)
+
+        view = discord.ui.View(timeout=None)
+
+        for answer in answers:
+            view.add_item(RoleButton(answer, selected_model))
+            print(f'added {answer} to view (correct answer: {selected_model})')
+
+        await ctx.respond(embed=embed, view=view)
+
 def setup(bot):
     bot.add_cog(Quiz(bot))
